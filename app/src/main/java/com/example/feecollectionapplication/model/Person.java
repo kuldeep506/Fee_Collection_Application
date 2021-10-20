b2a62f66316e16123418e8a191cb3f7798ababb0
Kuldeep Singh <hard31259@gmail.com>
2021-10-20 14:57:21 +0530

    added Person.java class

---
+++ app/src/main/java/com/example/feecollectionapplication/model/Person.java
@@ -0,0 +1,12 @@
+/*
+ Created by Intellij IDEA
+ Author Name: KULDEEP SINGH (kuldeep506)
+ Date: 20-10-2021
+*/
+
+package com.example.feecollectionapplication.model;
+
+public class Person {
+    private String name;
+
+}
